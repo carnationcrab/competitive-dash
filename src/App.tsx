d6481528e00e40cc6c98feea0b2e96c8ba55619f
Samantha Martorana <samanthamartorana7@gmail.com>
2020-10-10 14:54:44 -0500

    Add material UI

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,15 @@ function App() {
         >
           Learn React
         </a>
+
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
       </header>
     </div>
   );
